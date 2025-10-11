72674601a6c465611f87fe15d1170dad9e4daabd
Érico <oliv.ericorib@gmail.com>
2025-10-10 22:43:23 -0300

    feat: otimizar query JPQL

--- src/main/java/com/erico/lavanderia/domain/scheduling/SchedulingRepository.java
+++ src/main/java/com/erico/lavanderia/domain/scheduling/SchedulingRepository.java
@@ -2,6 +2,7 @@ package com.erico.lavanderia.domain.scheduling;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,5 +13,6 @@ public interface SchedulingRepository extends JpaRepository<Scheduling, UUID> {
 
     boolean existsByDateTimeAndUserId(SchedulingDateTime dateTime, UUID userId);
 
-    List<Scheduling> findByUserId(UUID userId);
+    @Query("SELECT s FROM Scheduling s LEFT JOIN User u ON s.user.id = u.id WHERE u.id = :userId")
+    List<Scheduling> findByUserId(@Param("userId") UUID userId);
 }
